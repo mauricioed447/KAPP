e39ea0022ce6cc8034ff337afdfc58e83118572d
mauricioed447 <mmacorreo1@gmail.com>
2025-06-03 13:18:31 -0400

    Delete components directory

--- components/common/RobotIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const RobotIcon = ({ className = "w-10 h-10" }) => (
-  <div className={`${className} bg-gradient-to-br from-blue-400 to-purple-500 rounded-full flex items-center justify-center text-white font-bold text-lg`}>
-    🤖
-  </div>
-);
